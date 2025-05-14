bde8197f236af78d05d2aa73c8c3ca6bf34e14a9
Gouravpatel007 <144343775+Gouravpatel007@users.noreply.github.com>
2025-05-14 23:39:59 +0530

    Delete projecteco directory

--- projecteco/server/utils/generateToken.js
+++
@@ -1,9 +0,0 @@
-import jwt from 'jsonwebtoken';
-
-const generateToken = (id) => {
-  return jwt.sign({ id }, process.env.JWT_SECRET || 'yourdefaultsecret' ,{
-    expiresIn: '30d'
-  });
-};
-
-export default generateToken;
