857bb463e5ca20ff247527440ad873ea40c9b186
Gouravpatel007 <144343775+Gouravpatel007@users.noreply.github.com>
2026-02-07 16:48:17 +0530

    Update paymentRoutes.js

--- server/routes/paymentRoutes.js
+++ server/routes/paymentRoutes.js
@@ -7,18 +7,3 @@ router.post('/create-order', createOrder)
 
 export default router;
 
-
-
-
-
-// // routes/paymentRoutes.js
-// import express from 'express';
-// const router = express.Router();
-
-// // Sample route for payments
-// router.post('/create', (req, res) => {
-//   res.send('Payment created');
-// });
-
-// // ✅ This is the key part
-// export default router;
